c2f0f38e92c09c0a6051431dc28fb0538eb24824
Jimmy Blundell <jblundell123@gmail.com>
2020-11-10 21:18:09 -0600

    Finished first portion of assignment

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,12 +4,12 @@ from keras.preprocessing.text import Tokenizer
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from keras.callbacks import TensorBoard
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers.embeddings import Embedding
 from keras.layers import Flatten
 import matplotlib.pyplot as plt
-from keras import callbacks
 
 # test comment
 
@@ -23,6 +23,8 @@ tokenizer = Tokenizer(num_words=2000)
 tokenizer.fit_on_texts(sentences)
 # getting the vocabulary of data
 sentences = tokenizer.texts_to_matrix(sentences)
+padded = pad_sequences(sentences, maxlen=2470) # Max review len turned out to be 2470
+
 
 le = preprocessing.LabelEncoder()
 y = le.fit_transform(y)
@@ -31,7 +33,30 @@ X_train, X_test, y_train, y_test = train_test_split(sentences, y, test_size=0.25
 # Number of features
 # print(input_dim)
 model = Sequential()
-model.add(layers.Dense(300, input_dim=input_dim, activation='relu'))
-model.add(layers.Dense(3, activation='sigmoid'))
+model.add(layers.Dense(300, input_dim=2000, activation='relu')) # fixed error: specified input_dim parameter
+model.add(layers.Dense(3, activation='softmax')) # fixed error: changed from sigmoid to softmax
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
 history = model.fit(X_train, y_train, epochs=5, verbose=True, validation_data=(X_test, y_test), batch_size=256)
+
+# plot accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Training and Validation Accuracy')
+plt.xlabel('# epochs')
+plt.ylabel('Accuracy')
+plt.legend(['Training', 'Validation'], loc='upper right')
+plt.show()
+
+# plot loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Training and Validation Loss')
+plt.xlabel('# epochs')
+plt.ylabel('Loss')
+plt.legend(['Training', 'Validation'], loc='upper right')
+plt.show()
+
+
+
+
+
